cbc1d31fba1f2f5d19a93923e193c26d7be20e41
jackbroadbent <jacksonmbroadbent@gmail.com>
2025-01-07 21:29:25 -0500

    Create listImages.js

---
+++ netlify/functions/listImages.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async () => {
+    try {
+        // Path to the images folder
+        const imagesFolder = path.join(__dirname, '../../images');
+        const files = fs.readdirSync(imagesFolder);
+
+        // Filter only image files
+        const imageFiles = files.filter(file =>
+            /\.(jpg|jpeg|png|gif)$/i.test(file)
+        );
+
+        // Return the list of image paths
+        return {
+            statusCode: 200,
+            body: JSON.stringify(imageFiles),
+        };
+    } catch (error) {
+        console.error('Error listing images:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Unable to list images' }),
+        };
+    }
+};
